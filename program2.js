43e708c891bc5b7292151d06f4f899358e136978
Mayur <mayurjaiswal999@gmail.com>
2024-04-25 14:27:00 +0530

    final commit

--- program2.js
+++ program2.js
@@ -1,6 +1,24 @@
 function smallestMissingPositiveInteger(nums) {
+  nums = nums.filter(num => num > 0);
 
-  // Implement the function smallest_missing_positive_integer
+
+  for (let i = 0; i < nums.length; i++) {
+      let index = Math.abs(nums[i]) - 1;
+      if (index < nums.length && nums[index] > 0) {
+          nums[index] = -nums[index];
+      }
+  }
+
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > 0) {
+          return i + 1;
+      }
+  }
+
+ 
+  return nums.length + 1;
+
 
 }
 
